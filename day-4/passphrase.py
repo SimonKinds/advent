5d285c6b868b92fdea45d1c63494e87911095fe7
Simon Kindström <simon.kindstrom@gmail.com>
2017-12-04 12:37:01 +0100

    Day 4: Part two

--- day-4/passphrase.py
+++ day-4/passphrase.py
@@ -7,6 +7,14 @@ def is_passphrase_valid(passphrase):
             return False
     return True
 
-def count_valid_phrases_from_file(filename):
+def is_passphrase_valid_anagram(passphrase):
+    counter = Counter([frozenset(Counter(chars)) for chars in list(passphrase.split())])
+    for word_counter, count in counter.items():
+        if count > 1:
+            return False
+    return True
+
+
+def count_valid_phrases_from_file(filename, fun):
     with open(filename) as f:
-        return sum([is_passphrase_valid(line) for line in f.readlines()])
+        return sum([fun(line) for line in f.readlines()])
--- day-4/passphrase_test.py
+++ day-4/passphrase_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from passphrase import is_passphrase_valid
+from passphrase import is_passphrase_valid, is_passphrase_valid_anagram
 
 class PassphraseTestCase(unittest.TestCase):
     def test_single_ok(self):
@@ -8,3 +8,7 @@ class PassphraseTestCase(unittest.TestCase):
 
     def test_repeating_false(self):
         self.assertFalse(is_passphrase_valid('a a'))
+
+    def test_anagram(self):
+        self.assertTrue(is_passphrase_valid_anagram('abcde fghij'))
+        self.assertFalse(is_passphrase_valid_anagram('abcde xyz ecdab'))
